3a1baba98a76115c7ae37df616d4600e8959cd90
John Craddock <jtcraddock@gmail.com>
2018-07-17 23:52:35 +0100

    implementation

--- file.js
+++ file.js
@@ -1,9 +1,23 @@
 //Calculate 2d distance
 
 //declare two points
+a = {
+			x:0,
+			y:0
+		}
+
+b = {
+			x:2,
+			y:2
+		}
+
 
 //get squares
+x2 = x*x
+y2 = y*y
 
 //get sum  of squares
+sum_of_squares = x2+y2
 
 //get square root of sum
+result = sum_of_squares^0.5
